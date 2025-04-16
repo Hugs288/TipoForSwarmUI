725fdb5b92ffb40b60de63a3fd333ef22268c3a8
Hugs288 <131965855+Hugs288@users.noreply.github.com>
2025-04-17 00:49:34 +0330

    Delete TIPOExtension directory

--- TIPOExtension/assets/TIPO.js
+++
@@ -1,6 +0,0 @@
-postParamBuildSteps.push(() => {
-    let TIPOParamGroup = document.getElementById('input_group_content_tipopromptgeneration');
-    if (TIPOParamGroup && !currentBackendFeatureSet.includes('tipo_prompt_generation')) {
-        TIPOParamGroup.append(createDiv(`tipo_prompt_generation_install_button`, 'keep_group_visible', `<button class="basic-button" onclick="installFeatureById('tipo_prompt_generation', 'tipo_prompt_generation_install_button')">Install TIPO</button>`));
-    }
-});
